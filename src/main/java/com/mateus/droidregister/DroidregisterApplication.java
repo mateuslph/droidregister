ad817ac4c8996c2592be34a1ba169d8d0eaa8dec
mateuslph <mlptche@gmail.com>
2024-01-09 19:36:20 -0300

    initialized project

---
+++ src/main/java/com/mateus/droidregister/DroidregisterApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.droidregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroidregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroidregisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/droidregister/DroidregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.droidregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroidregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
